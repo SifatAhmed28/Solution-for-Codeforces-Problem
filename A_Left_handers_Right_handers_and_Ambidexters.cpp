9e9d6e2c1931144a54ce32bc90991bdc5aed0d27
Sifat Ahmed <151382725+SifatAhmed28@users.noreply.github.com>
2024-10-05 08:02:08 +0600

    A_Left_handers_Right_handers_and_Ambidexters

--- A_Left_handers_Right_handers_and_Ambidexters.cpp
+++ A_Left_handers_Right_handers_and_Ambidexters.cpp
@@ -1,33 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main(){
-    int l,r,a;
-    cin>>l>>r>>a;
-    if(a==0 && (l==0 || r==0) ){
-        cout<<0;
-    }
-    else{
-    while(a !=0){
-        if(l%2 !=0){
-            l++;
-            a--;
-        }
-        else if(r%2 !=0){
-            r++;
-            a--;
-        }
-        else if(l>r){
-            r++;
-            a--;
-        }
-        else {
-            l++;
-            a--;
-        }
-       
 
-    }
-    cout<<((l)+(r))-1;
+int main() {
+    int l, r, a;
+    cin >> l >> r >> a;
+
+    // If both left and right are zero, and a is zero, the result is zero.
+    if (l == 0 && r == 0 && a == 0) {
+        cout << 0;
+        return 0;
     }
 
+    // First, balance the pairs by making the smaller one equal to the larger.
+    while (a > 0 && l != r) {
+        if (l < r) l++;
+        else r++;
+        a--;
     }
+
+    // After balancing, divide remaining 'a' equally between both sides.
+    l += a / 2;
+    r += a / 2;
+
+    // The largest possible balanced score is twice the smaller of the two values.
+    cout << 2 * min(l, r);
+    return 0;
+}
+
